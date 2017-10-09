260baa5ca9e8fad141688bfff84b8855b2573a60
Yinyin L <yinyinl@gmail.com>
2017-10-10 00:11:37 +0800

    database purge program

---
+++ cmd/purger/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/yinyin/go-maildock/cmd"
+	"log"
+	"time"
+	"context"
+)
+
+
+func main() {
+	cfg, err := cmd.LoadConfigurationWithFlags()
+	if nil != err {
+		log.Printf("cannot load configuration: %v", err)
+		return
+	}
+	if cfg.PurgeDays < 1 {
+		log.Printf("configurated purge-days less than 1: %v", cfg.PurgeDays)
+		return
+	}
+	purgeDuration := time.Duration(cfg.PurgeDays * 24) * time.Hour
+	dbconn, err := cfg.Database.Config.OpenConnection()
+	if nil != err {
+		log.Printf("failed on connection to database: %v", err)
+		return
+	}
+	defer dbconn.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Hour)
+	defer cancel()
+	if err = dbconn.PurgeMail(ctx, purgeDuration); nil != err {
+		log.Printf("failed on purging mail: %v", err)
+		return
+	}
+}
+
